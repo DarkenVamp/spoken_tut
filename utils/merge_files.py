3b83be34376482d3428baedbf74283c34a35efa6
Hitesh Deswal <hiteshd1207@gmail.com>
2022-02-09 23:10:38 +0530

    utils: merge_files: add safeguards for media streams
    * 's?' allows empty subtitle streams
    * 'v:0' and 'a:0' selects first video and audio streams respectively

--- utils/merge_files.py
+++ utils/merge_files.py
@@ -7,10 +7,13 @@ def merge_aud_vid(video_name, audio_name, output_name):
     audio_stream = ffmpeg.input(audio_name)
 
     # strip audio
-    video_stream, subs_stream = video_stream['v'], video_stream['s']
+    video_stream, subs_stream = video_stream['v:0'], video_stream['s?']
+    audio_stream = audio_stream['a:0']
 
     # highly compatible with most devices
     video_codec = 'libx264'
+    # mp4 container only supports mov_text
+    subs_codec = 'mov_text'
     # crf > 24 is enough compression as its a spoken tutorial
     output_crf = 25
 
@@ -22,5 +25,5 @@ def merge_aud_vid(video_name, audio_name, output_name):
         filename=output_name,  # output name
         crf=output_crf,  # for compression
         vcodec=video_codec,  # for compatibility
-        scodec='mov_text',  # mp4 supports only mov_text subtitle codec
+        scodec=subs_codec,  # needed for mp4
     ).run(overwrite_output=True)
