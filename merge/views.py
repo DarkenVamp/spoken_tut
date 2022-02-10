af6104db54c57ef0fa7e4cd15c88efe8e29fbf3e
Hitesh Deswal <hiteshd1207@gmail.com>
2022-02-10 17:09:13 +0530

    views: use source name directly instead of relying on fs.save()

--- merge/views.py
+++ merge/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from utils.merge_files import merge_aud_vid
 from utils.text_audio import text_to_audio_file
+from spoken_tut.settings import MEDIA_ROOT, MEDIA_URL
+import os
 
 
 def merge_home(request):
@@ -19,23 +21,25 @@ def merge_home(request):
     except KeyError:
         return render(request, 'merge/merge_page.html', {'error': 'Upload both files'})
 
-    v_name = fs.save(vid.name, vid)
-    a_name = fs.save(aud.name, aud)
     # rsplit('.', maxsplit=1) gives ['filename', 'extension']
-    op_path = 'media/' + v_name.rsplit('.', maxsplit=1)[0] + '-merged.mp4'
+    op_url = MEDIA_URL + vid.name.rsplit('.', maxsplit=1)[0] + '-merged.mp4'
 
-    # [1:] removes leading '/' character so relative path can be used
-    v_url = fs.url(v_name)[1:].replace('%20', ' ')
-    a_url = fs.url(a_name)[1:].replace('%20', ' ')
+    # save files to access them later via ffmpeg
+    fs.save(vid.name, vid)
+    fs.save(aud.name, aud)
+
+    # generate paths to pass to ffmpeg
+    v_path = os.path.join(MEDIA_ROOT, vid.name)
+    a_path = os.path.join(MEDIA_ROOT, aud.name)
     try:
-        merge_aud_vid(v_url, a_url, op_path)
+        merge_aud_vid(v_path, a_path, op_url)
     except:
         return render(request, 'merge/merge_page.html', {'error': 'Incompatible media files'})
 
     # delete processed files to save space
     fs.delete(vid.name)
     fs.delete(aud.name)
-    return render(request, 'merge/merge_page.html', {'result': op_path})
+    return render(request, 'merge/merge_page.html', {'result': op_url})
 
 
 def tts_home(request):
@@ -50,13 +54,14 @@ def tts_home(request):
     except KeyError:
         return render(request, 'merge/tts_page.html', {'error': 'Missing fields'})
 
-    local_txt_name = fs.save(txt_file.name, txt_file)
-    audio_path = 'media/' + local_txt_name.rsplit('.', maxsplit=1)[0] + '.wav'
+    fs.save(txt_file.name, txt_file)
+    txt_path = os.path.join(MEDIA_ROOT, txt_file.name)
+    audio_url = MEDIA_URL + txt_file.name.rsplit('.', maxsplit=1)[0] + '.wav'
 
     try:
-        text_to_audio_file(fs.url(local_txt_name)[1:], audio_path, gender)
+        text_to_audio_file(txt_path, audio_url, gender)
     except:
         return render(request, 'merge/tts_page.html', {'error': 'Invalid text format'})
 
     fs.delete(txt_file.name)
-    return render(request, 'merge/tts_page.html', {'result': audio_path})
+    return render(request, 'merge/tts_page.html', {'result': audio_url})
