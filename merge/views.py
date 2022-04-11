599cad959b6d42ca238122cb2fc0f2edcbfe3684
Hitesh Deswal <hiteshd1207@gmail.com>
2022-04-11 20:27:24 +0530

    templates, views: unify merge and tts pages

--- merge/views.py
+++ merge/views.py
@@ -17,60 +17,39 @@ def merge_home(request):
     # dictionary keys taken from merge_page template
     try:
         vid = request.FILES['video']
-        aud = request.FILES['audio']
+        csv_file = request.FILES['transcript']  # from file input
+        gender = int(request.POST['gender'])  # from radio input, gives 0 or 1
+        # from text input, default is 200, default value is '' so set it to 200
+        rate = int(request.POST['rate'] or 200)
     except KeyError:
-        return render(request, 'merge/merge_page.html', {'error': 'Upload both files'})
+        return render(request, 'merge/merge_page.html', {'error': 'Missing fields'})
 
     # rsplit('.', maxsplit=1) gives ['filename', 'extension']
     op_url = MEDIA_URL + vid.name.rsplit('.', maxsplit=1)[0] + '-merged.mp4'
 
     # save files to access them later via ffmpeg
     fs.save(vid.name, vid)
-    fs.save(aud.name, aud)
+    fs.save(csv_file.name, csv_file)
 
     # generate paths to pass to ffmpeg
+    csv_path = os.path.join(MEDIA_ROOT, csv_file.name)
+    audio_url = MEDIA_URL + csv_file.name.rsplit('.', maxsplit=1)[0]
     v_path = os.path.join(MEDIA_ROOT, vid.name)
-    a_path = os.path.join(MEDIA_ROOT, aud.name)
+    a_path = audio_url + '.wav'
+
     try:
+        # generate audio
+        generate_audio(csv_path, audio_url, gender, rate)
+        # merge video with generated audio
         merge_aud_vid(v_path, a_path, op_url)
+        # delete generated audio after processing
+        os.remove(a_path)
         context = {'result': op_url}
     except Exception as e:
         context = {'error': e}
     finally:
         # delete files to save space
         fs.delete(vid.name)
-        fs.delete(aud.name)
-
-    return render(request, 'merge/merge_page.html', context=context)
-
-
-def tts_home(request):
-    # for handling GET and other requests
-    if request.method != 'POST':
-        return render(request, 'merge/tts_page.html')
-
-    fs = FileSystemStorage()
-
-    try:
-        csv_file = request.FILES['transcript']  # from file input
-        gender = int(request.POST['gender'])  # from radio input, gives 0 or 1
-        # from text input, default is 200, default value is '' so set it to 200
-        rate = int(request.POST['rate'] or 200)
-    except KeyError:
-        return render(request, 'merge/tts_page.html', {'error': 'Missing fields'})
-
-    # save() also creates media directory so avoids FileNotFound
-    fs.save(csv_file.name, csv_file)
-    csv_path = os.path.join(MEDIA_ROOT, csv_file.name)
-    audio_url = MEDIA_URL + csv_file.name.rsplit('.', maxsplit=1)[0]
-
-    try:
-        generate_audio(csv_path, audio_url, gender, rate)
-        context = {'result': audio_url + '.wav'}
-    except Exception as e:
-        context = {'error': e}
-    finally:
-        # nuke file
         fs.delete(csv_file.name)
 
-    return render(request, 'merge/tts_page.html', context=context)
+    return render(request, 'merge/merge_page.html', context=context)
